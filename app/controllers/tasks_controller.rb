4a2bc752ee1fff0ee4c341fd48020862ad82a8f0
great084 <great084@gmail.com>
2020-04-05 13:30:14 +0900

    Task 詳細表示機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,11 @@ class TasksController < ApplicationController
     def index
         @tasks = Task.all
     end
-    
+
+    def show 
+        @task = Task.find(params[:id])
+    end
+
     private
 
     def task_parameter
