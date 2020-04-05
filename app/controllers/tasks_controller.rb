ab2783cee309d7300364247effcbd728043640af
great084 <great084@gmail.com>
2020-04-05 13:03:10 +0900

    Task 新規投稿機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,16 @@
 class TasksController < ApplicationController
+
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        Task.create(task_parameter)
+    end
+
+    private
+
+    def task_parameter
+        params.require(:task).permit(:title, :content)
+    end
 end
